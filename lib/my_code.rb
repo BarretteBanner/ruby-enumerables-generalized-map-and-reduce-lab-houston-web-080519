8c8da7ed242f3c7e948d1595979e8ef75d636b99
Barrette Banner <barrettebanner@gmail.com>
2019-07-10 03:01:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,8 +14,9 @@ def reduce(array)
   counter = 0 
   total = 0  
   while counter < array.length do
-     total = yield(array, total)
+     yield(array, total)
     counter += 1 
   end
+  total = yield(array, total)
   return total
 end
