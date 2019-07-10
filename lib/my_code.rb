6649c6c140326fb0c45ac0d6b533cee0fb940315
Barrette Banner <barrettebanner@gmail.com>
2019-07-10 02:16:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(array)
+  counter = 0
+  new_array = []
+  while counter < array.length do 
+   new_array.push(yield(array[counter]))
+    counter += 1
+  end 
+  return new_array
+end  
+
+
+def reduce(array, start = 0 )
+  counter = 0 
+  new_total = start 
+  while counter < array.length do
+    new_total += yield(array[counter])
+    counter += 1 
+  end
+  return new_total
+end
